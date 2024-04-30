805ac61d9a7114a004fe2a533e0563a38d8d7881
m7-startpn <mauricio@startpn.com>
2024-04-30 11:11:19 -0300

    add: Login screen and remove screens that will not be used

--- src/modules/Auth/pages/Login/index.js
+++ src/modules/Auth/pages/Login/index.js
@@ -3,14 +3,12 @@ import { useMediaQuery } from 'react-responsive';
 import { useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 
-import TwoColumnLayout from '../../../../layouts/Desktop/TwoColumnLayout';
 import OneColumnLayoutMobile from '../../../../layouts/Mobile/OneColumnLayoutMobile';
 
 import Button from '../../../../components/Button';
 import Input from '../../../../components/Input';
 import InputPassword from '../../../../components/InputPassword';
 
-import ImageLogin from '../../../../images/svg/ImageLogin';
 import Logo from '../../../../images/svg/Logo';
 
 const Login = () => {
@@ -92,68 +90,50 @@ const Login = () => {
      }
     />
    ) : (
-    <TwoColumnLayout
-     page={'Login'}
-     column1={
-      <div className='h-full w-full flex items-center justify-center'>
-       <ImageLogin />
+    <div className='flex flex-col justify-center items-center h-screen px-4 w-100'>
+     <div className='w-4/12'>
+      <div className='mb-10'>
+       <Logo />
       </div>
-     }
-     column2={
-      <>
-       <h1 className='text-font font-boldc text-3xl pb-4'>Faça o login</h1>
+      <h1 className='text-primar font-boldc text-3xl pb-4'>Dados de acesso</h1>
 
-       <form onSubmit={handleSubmit(postLogin)} className='flex flex-col'>
-        <Input
-         label='E-mail'
-         error={errors.email}
-         placeholder='Insira seu e-mail'
-         validate={{
-          required: 'Campo obrigatório!',
-          pattern: {
-           value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-           message: 'E-mail inválido!',
-          },
-         }}
-         registerKey={'email'}
-         register={register}
-        />
+      <form onSubmit={handleSubmit(postLogin)} className='flex flex-col'>
+       <Input
+        label='E-mail'
+        error={errors.email}
+        placeholder='Insira seu e-mail'
+        validate={{
+         required: 'Campo obrigatório!',
+         pattern: {
+          value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+          message: 'E-mail inválido!',
+         },
+        }}
+        registerKey={'email'}
+        register={register}
+       />
 
-        <InputPassword
-         label='Senha'
-         placeholder='8+ Caracteres'
-         validate={{
-          required: 'Campo obrigatório!',
-          minLength: {
-           value: 8,
-           message: 'Mínimo 8 caracteres!',
-          },
-         }}
-         registerKey={'password'}
-         register={register}
-         error={errors.password}
-        />
+       <InputPassword
+        label='Senha'
+        placeholder='Insira sua senha'
+        validate={{
+         required: 'Campo obrigatório!',
+         minLength: {
+          value: 8,
+          message: 'Mínimo 8 caracteres!',
+         },
+        }}
+        registerKey={'password'}
+        register={register}
+        error={errors.password}
+       />
 
-        <div className='flex justify-between pt-4'>
-         <Button name='Login' width='w-40' height={'h-12'} textColor={'text-white'} backgroundColor='bg-blue1' type='submit' />
-
-         <div className='font-regularc text-font flex justify-end items-center'>
-          <p onClick={() => navigate('/lostpassword')} className='cursor-pointer'>
-           Esqueceu a senha?
-          </p>
-         </div>
-        </div>
-       </form>
-
-       <p className='font-regularc text-font text-left pt-4 text-label text-colors-50'>
-        Não é cadastrado?
-        <span onClick={() => navigate('/register')} className='text-blue1 cursor-pointer pl-2'>
-         Cadastre-se
-        </span>
-       </p>
-      </>
-     }
-    />
+       <div className='flex justify-between pt-4'>
+        <Button name='Entrar' width='w-100' height={'h-12'} textColor={'text-white'} backgroundColor='bg-primar' type='submit' />
+       </div>
+      </form>
+     </div>
+    </div>
    )}
   </>
  );
