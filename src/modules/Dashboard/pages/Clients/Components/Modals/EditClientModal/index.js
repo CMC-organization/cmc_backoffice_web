1ab1323fb5fbabab55f0d62b766c37f8a8908fc2
gstvcampos <gustavo.bmc76@gmail.com>
2024-05-14 11:33:49 -0300

    feat: new client modal

--- src/modules/Dashboard/pages/Clients/Components/Modals/EditClientModal/index.js
+++ src/modules/Dashboard/pages/Clients/Components/Modals/EditClientModal/index.js
@@ -1,8 +1,28 @@
-import { useClients } from '../../../../../../../context/ClientsContext';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+
 import XClose from '../../../../../../../images/svg/XClose';
+import AvatarIcon from '../../../../../../../images/svg/AvatarIcon';
+import CamIcon from '../../../../../../../images/svg/CamIcon';
+
+import Button from '../../../../../../../components/Button';
+import Input from '../../../../../../../components/Input';
+
+import { useClients } from '../../../../../../../context/ClientsContext';
 
 const EditClientModal = () => {
- const { setOpenEdit } = useClients();
+ const { openEdit, setOpenEdit } = useClients();
+  const [avatar, setAvatar] = useState(null);
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+   } = useForm();
+
+  const handleAvatarChange = (event) => {
+    setAvatar(event.target.files[0])
+  };
 
  const onCloseModal = (e) => {
   if (e.target.id === 'modalContainer') {
@@ -10,27 +30,105 @@ const EditClientModal = () => {
   }
  };
 
- return (
-    <div className='w-screen h-screen flex items-center justify-center fixed bg-modal  top-0 left-0 z-20' id='modalContainer' onClick={onCloseModal}>
-     <div className='w-[585px] min-h-80 bg-offWhite rounded-3xl  z-20 px-6 py-6  drop-shadow-md shadow-slate-700'>
-       <div className='flex items-center justify-between'>
+ return openEdit && (
+    <div className='w-screen h-screen flex items-center justify-center fixed bg-modal top-0 left-0 z-20' id='modalContainer' onClick={onCloseModal}>
+     <div className='w-[605px] min-h-[566px] bg-offWhite rounded-3xl z-20 drop-shadow-md shadow-slate-700'>
+       
+      <div className='flex items-center justify-between p-6'>
         <div className='flex items-center gap-3'>
+          <div
+            className='cursor-pointer'
+            onClick={() => {
+              setOpenEdit(false);
+            }}
+          >
+            <XClose />
+          </div>
+          <p className='text-lg text-black font-medium'>Editar cliente</p>
+        </div>
 
-         <p className='text-lg text-black font-medium'>Editar cliente</p>
+        <Button name='Editar' width={'w-28'} height={'h-8'} backgroundColor='bg-primar' fontSize={'text-sm'} type='submit' />
+      </div>
+      <div className='border-t border-border'>
+        <div className='flex justify-center py-10'>
+          <div className='relative'>
+            <label htmlFor='avatar' className='relative cursor-pointer bg-red4 rounded-full overflow-hidden h-24 w-24'>
+              {avatar ? <img alt="avatar do cliente" className='w-full h-full object-fill' src={URL.createObjectURL(avatar)} /> : <AvatarIcon className="mx-3 my-2" />}
+            </label>
+            <div className="absolute bg-primar bottom-1 right-1 rounded-full p-1 border-2 border-white">
+              <CamIcon />
+            </div>
+          </div>
+          <input 
+            type="file" 
+            name="avatar" 
+            id="avatar"
+            className='hidden'
+            onChange={handleAvatarChange} 
+          />
         </div>
-        <div
-          className='cursor-pointer'
-          onClick={() => {
-            setOpenEdit(false);
-          }}
-         >
-          <XClose />
-         </div>
+
+        <div className='grid grid-cols-2 gap-4 p-6'>
+          <div>
+            <Input
+              label="Razão social"
+              error={errors.name}
+              placeholder="Insira a razão social"
+              validate={{
+                required: 'Campo obrigatório!',
+              }}
+              registerKey={'name'}
+              register={register}
+            />
+            <Input
+              label="E-mail do financeiro"
+              error={errors.mail}
+              placeholder="Insira o e-mail"
+              validate={{
+                required: 'Campo obrigatório!',
+              }}
+              registerKey={'mail'}
+              register={register}
+            />
+            <Input
+              label="Créditos para compra"
+              error={errors.credits}
+              placeholder="Insira os creditos"
+              validate={{
+                required: 'Campo obrigatório!',
+              }}
+              registerKey={'credits'}
+              register={register}
+            />
+          </div>
+
+          <div>
+            <Input
+              label="CNPJ"
+              error={errors.cnpj}
+              placeholder="Insira o CNPJ"
+              validate={{
+                required: 'Campo obrigatório!',
+              }}
+              registerKey={'cnpj'}
+              register={register}
+            />
+            <Input
+              label="Senha de acesso"
+              error={errors.password}
+              placeholder="Nova senha"
+              validate={{
+                required: 'Campo obrigatório!',
+              }}
+              registerKey={'password'}
+              register={register}
+            />
+          </div>
         </div>
-       <div className='min-w-full min-h-[0.5px] my-3 bg-border' />
+      </div>
      </div>
     </div>
    )
-};
+}
 
 export default EditClientModal;
