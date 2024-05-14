0bff1344db800c58e05a348121984d709e86a053
m7-startpn <mauricio@startpn.com>
2024-05-14 14:10:43 -0300

    add: develop

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import { ToastContainer } from 'react-toastify';
 import 'primereact/resources/primereact.min.css';
 import 'primereact/resources/themes/fluent-light/theme.css';
 
-import reportWebVitals from './reportWebVitals';
-import AppStack from './routes/AppStack';
 import { PrimeReactProvider } from 'primereact/api';
 import { GlobalProvider } from './context/GlobalContext';
-
-import './index.css';
 import { OrdersProvider } from './context/OrdersContext';
 import { ProductsProvider } from './context/ProductsContext';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import AppStack from './routes/AppStack';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
