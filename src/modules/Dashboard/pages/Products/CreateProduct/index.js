cf3312c7f4c2e507bdd3c4587fc1860c495f8c00
gstvcampos <gustavo.bmc76@gmail.com>
2024-05-16 14:41:38 -0300

    fix: rule

--- src/modules/Dashboard/pages/Products/CreateProduct/index.js
+++ src/modules/Dashboard/pages/Products/CreateProduct/index.js
@@ -153,52 +153,56 @@ const onSubmit = (data) => {
          <p className='text-base text-black font-medium'>Variações</p>
         </div>
         <div className='px-4 overflow-auto h-96'>
-           {fields.map((field, index) => (
+          {fields.map((field, index) => (
             <div key={field.id} className='flex border-b py-4'>
-                     <div className='flex items-start pr-4 pt-7'>
-                      <span className='bg-primar rounded-full px-3 py-2 text-white font-medium text-xl'>{index+1}</span>
-                     </div>
-                     <div>
-             <div>
-              <div className='flex gap-3'>
-               <Input
-                name={`variables[${index}].variable`}
-                label='Nome da variavel'
-                error={errors.variables && errors.variables[index] && errors.variables[index].variable}
-                placeholder='Insira o nome do variavel'
-                control={control}
-               />
-               <button className='pb-2' onClick={() => handleRemoveVariable(index)}>
-                <TrashIcon />
-               </button>
+              <div className='flex items-start pr-4 pt-7'>
+                <span className='bg-primar rounded-full px-3 py-2 text-white font-medium text-xl'>{index+1}</span>
               </div>
-              <div className='pb-4'>
-               <label htmlFor={`icon${index}`} className='cursor-pointer rounded-full overflow-hidden h-11 w-full border-2 border-dashed flex justify-center'>
-                {icons[index] ? (
-                 <img alt='icone do produto' className='w-full h-full object-fill' src={URL.createObjectURL(icons[index])} />
-                ) : (
-                 <div className='flex items-center text-sm font-medium'>
-                  <CloudIcon className='mx-3 my-2' />
-                  <span>Upload do icone</span>
-                 </div>
-                )}
-               </label>
-               <input type='file' name={`variables[${index}].icon`} id={`icon${index}`} className='hidden' onChange={(event) => handleIconChange(event, index)} />
+
+              <div>
+                <div className='flex gap-3'>
+                  <Input
+                    label='Nome da variavel'
+                    name={`variables[${index}].variable`}
+                    control={control}
+                    placeholder='Insira o nome do variavel'
+                    rules={{
+                      required: 'Campo obrigatório!',
+                    }}
+                    error={errors.variables && errors.variables[index] && errors.variables[index].variable}
+                  />
+                  <button className='pb-2' onClick={() => handleRemoveVariable(index)}>
+                    <TrashIcon />
+                  </button>
+                </div>
+
+                <div className='pb-4'>
+                  <label htmlFor={`icon${index}`} className='cursor-pointer rounded-full overflow-hidden h-11 w-full border-2 border-dashed flex justify-center'>
+                    {icons[index] ? (
+                    <img alt='icone do produto' className='w-full h-full object-fill' src={URL.createObjectURL(icons[index])} />
+                    ) : (
+                    <div className='flex items-center text-sm font-medium'>
+                      <CloudIcon className='mx-3 my-2' />
+                      <span>Upload do icone</span>
+                    </div>
+                    )}
+                  </label>
+                  <input type='file' name={`variables[${index}].icon`} id={`icon${index}`} className='hidden' onChange={(event) => handleIconChange(event, index)} />
+                </div>
+
+                <div className='flex gap-4'>
+                  <Input name={`variables[${index}].option`} label='Opção' error={errors.variables && errors.variables[index] && errors.variables[index].option} placeholder='Insira o nome' control={control} />
+                  <Input name={`variables[${index}].value`} label='Valor adicional' error={errors.variables && errors.variables[index] && errors.variables[index].value} placeholder='0,00' control={control} />
+                </div>
+                <button className='pb-3' type='button'>
+                  <span className='text-primar font-medium text-base'>+ Adicionar opção</span>
+                </button>
               </div>
-              <div className='flex gap-4'>
-               <Input name={`variables[${index}].option`} label='Opção' error={errors.variables && errors.variables[index] && errors.variables[index].option} placeholder='Insira o nome' control={control} />
-               <Input name={`variables[${index}].value`} label='Valor adicional' error={errors.variables && errors.variables[index] && errors.variables[index].value} placeholder='0,00' control={control} />
-              </div>
-              <button className='pb-3' type='button'>
-               <span className='text-primar font-medium text-base'>+ Adicionar opção</span>
-              </button>
-             </div>
-          </div>
-         </div>
-           ))}
-            <button className='py-3' type='button' onClick={handleAddVariable}>
-                <span className='text-primar font-medium text-base'>+ Adicionar nova variavel</span>
-            </button>
+            </div>
+          ))}
+          <button className='py-3' type='button' onClick={handleAddVariable}>
+              <span className='text-primar font-medium text-base'>+ Adicionar nova variavel</span>
+          </button>
         </div>
        </div>
       </div>
